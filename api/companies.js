edc540a0de338b7609b36282cd459567736dfe00
Kuldeep S Bhakuni <62176452+devdotcore@users.noreply.github.com>
2023-05-11 14:27:59 +0100

    texst

---
+++ api/companies.js
@@ -0,0 +1,66 @@
+const axios = require('axios');
+const pako = require('pako');
+const cache = require('memory-cache');
+
+exports.handler = async (event, context) => {
+  try {
+    const gzUrl = 'https://data.worksponsors.co.uk/master.json.gz';
+    const cachedData = cache.get('jsonData');
+    let jsonData;
+
+    if (cachedData) {
+      jsonData = cachedData;
+    } else {
+      const response = await axios.get(gzUrl, { responseType: 'arraybuffer' });
+      const decompressedData = pako.inflate(response.data, { to: 'string' });
+      jsonData = JSON.parse(decompressedData);
+      cache.put('jsonData', jsonData);
+    }
+
+    const companyNamesParam = event.queryStringParameters.companyNames;
+    if (!companyNamesParam) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'Missing companyNames parameter' }),
+      };
+    }
+
+    const companyNames = companyNamesParam.split(',').map((name) => name.trim());
+
+    const results = companyNames.map((name) => {
+      const searchResults = searchByName(name, jsonData);
+      return {
+        [name]: {
+          key: name,
+          isMatchFound: searchResults.length > 0,
+          count: searchResults.length,
+        },
+      };
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
+};
+
+function searchByName(searchKey, refinedData) {
+  const escapedSearchKey = searchKey.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  const hashMap = {};
+
+  refinedData.forEach((item) => {
+    hashMap[item.name] = item;
+  });
+
+  const searchResults = Object.values(hashMap).filter((item) =>
+    item.name.match(new RegExp(`\\b${escapedSearchKey}\\b`, 'i'))
+  );
+
+  return searchResults;
+}
--- api/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/worksponsors/companies-api
-
-go 1.19
-
-require github.com/aws/aws-lambda-go v1.38.0
